903413f8f70b41c77a84d8e5825729c901b03611
Martarp83 <martaruizperez@alumno.ieselrincon.es>
2024-12-13 11:45:08 +0000

    Ultimos cambios

--- resources/views/messages.blade.php
+++ resources/views/messages.blade.php
@@ -11,19 +11,24 @@
         <h1>Prueba superada</h1>
         @if($messages->isEmpty())
             <p>No hay mensajes en la base de datos</p>
-        @else
+        @else 
+        
             <ul>
                 @foreach($messages as $message)
                     <li>{{ $message->text }}
-                    
-                        <!-- Mostrar imagen desde URL o una predeterminada -->
+
+                        
+                      <!-- Mostrar imagen desde URL o una predeterminada -->
                         <img src="{{ asset('storage/' . $message->imagen) }}" alt="{{ $message->name }}"
                             style="width: 50px; height: 50px; object-fit: cover; border-radius: 5px;">
+
                     </li>
                     
                 @endforeach
+ 
             </ul>
         @endif
+
     </div>
 </body>
 </html>
